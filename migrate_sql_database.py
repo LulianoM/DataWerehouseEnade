eac9201eaa4b72dc16792a2c4b3cde40eba24b1f
LulianoM <martinslucianofigueira@gmail.com>
2021-11-02 20:37:48 -0300

    script para migração de dados

--- migrate_sql_database.py
+++ migrate_sql_database.py
@@ -1,17 +1,30 @@
 import sqlite3
+import pandas as pd
+import tqdm
 
 
-conn = sqlite3.connect('dadosenade.db')
-cursor = conn.cursor()
 
-cursor.execute("""
-INSERT INTO clientes (nome, idade, cpf, email, fone, cidade, uf, criado_em)
-VALUES ('Matheus', 19, '33333333333', 'matheus@email.com', '11-98765-4324', 'Campinas', 'SP', '2014-06-08')
-""")
+def migrate_values(filepath):
+    conn = sqlite3.connect('dadosenade.db')
+    cursor = conn.cursor()
+    columns = ["NU_ANO", "CO_GRUPO", "CO_MODALIDADE", "CO_UF_CURSO", "CO_REGIÃO_CURSO", "NU_IDADE",
+    "TP_SEXO", "CO_TURNO_GRADUCAO", "QE_I02"]
+    df = pd.read_csv(filepath= filepath, sep= ";", usecols= columns)
+    for row in tqdm(df):
+        cursor.execute(f"""
+        INSERT INTO TABELA FATO (NU_ANO, CO_GRUPO, CO_MODALIDADE, CO_UF_CURSO, CO_REGIÃO_CURSO, NU_IDADE,TP_SEXO, CO_TURNO_GRADUCAO, QE_I02)
+        VALUES ({df["NU_ANO"][row]}, {df["CO_GRUPO"][row]}, {df["CO_MODALIDADE"][row]}, {df["CO_UF_CURSO"][row]}, {df["CO_REGIÃO_CURSO"][row]}, {df["NU_IDADE"][row]}, {df["TP_SEXO"][row]},{df["CO_TURNO_GRADUCAO"][row]}, {df["QE_I02"][row]})
+        """)
+        conn.commit()
+    conn.close()
 
 # gravando no bd
-conn.commit()
+print("\n Migrando dados 2017\n")
+migrate_values("DadosENADE/3.Dados/MICRODADOS_ENADE_2017.txt")
+print("\n Migrando dados 2018\n")
+migrate_values("DadosENADE/3.Dados/microdados_enade_2018.txt")
+print("\n Migrando dados 2019\n")
+migrate_values("DadosENADE/3.Dados/microdados_enade_2019.txt")
+
 
 print('Dados inseridos com sucesso.')
-
-conn.close()
